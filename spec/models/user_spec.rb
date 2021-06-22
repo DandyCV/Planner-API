92e7b4a8bfac8d7b400cc70e920e66abc528ccd1
Dandycv <dandycvc@gmail.com>
2021-06-22 20:59:56 +0300

    Add User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,10 @@
 # frozen_string_literal: true
 
-# RSpec.describe User, type: :model do
-#   describe '.create' do
-#     let(:user) { described_class.create(email: 'test@email.com', password: 'test1234') }
-
-#     it 'creates valid user' do
-#       expect(user).to be_valid
-#     end
-
-#     it 'does not allow blank email' do
-#       user.email = nil
-#       user.save
-#       expect(user.errors[:email]).to include("can't be blank")
-#     end
-
-#     it 'dones not allow blank password' do
-#       user.password = nil
-#       user.save
-#       expect(user.errors[:password]).to include("can't be blank")
-#     end
-
-#     it 'dones not allow users with same emails' do
-#       user.save
-#       user_with_same_email = described_class.create(email: 'test@email.com', password: 'test2345')
-#       expect(user_with_same_email.errors[:email]).to include('has already been taken')
-#     end
-#   end
-# end
+RSpec.describe User, type: :model do
+  it { expect(described_class).to be < ApplicationRecord }
+  it { is_expected.to have_attribute(:id) }
+  it { is_expected.to have_attribute(:email) }
+  it { is_expected.to have_secure_password }
+  it { is_expected.to have_attribute(:created_at) }
+  it { is_expected.to have_attribute(:updated_at) }
+end
