6d5414253761722debcd00d453a75b772da9a1ac
Dandycv <dandycvc@gmail.com>
2021-08-10 11:42:37 +0300

    Move token validation to operation

--- app/concepts/api/v1/users/confirmations/operation/confirm.rb
+++ app/concepts/api/v1/users/confirmations/operation/confirm.rb
@@ -3,17 +3,33 @@
 module Api::V1::Users::Confirmations::Operation
   class Confirm < ApplicationOperation
     step :decode_token
-    step :validate_data
+    step :validate_user
+    step :validate_token
     step :update_user
 
     def decode_token(params)
-      data = Api::V1::Lib::Service::TokenValidator.decode(params[:token])
-      data[:errors] ? Failure(data[:errors]) : Success(data.first)
+      data = Api::V1::Lib::Service::EmailToken.decode(params[:token])
+    rescue JWT::VerificationError
+      Failure({ errors: [{ token: I18n.t('users.confirmations.token.invalid') }] })
+    else
+      Success(data.first)
     end
 
-    def validate_data(data)
-      validated_data = Api::V1::Lib::Service::TokenValidator.user_validation(data)
-      validated_data.is_a?(::User) ? Success(validated_data) : Failure(validated_data)
+    def validate_user(data)
+      user = User.find_by(
+        id: data[:id],
+        email: data[:email],
+        created_at: data[:created_at]
+      )
+      user ? Success(user) : Failure({ errors: [{ token: I18n.t('users.confirmations.token.data') }] })
+    end
+
+    def validate_token(user)
+      if (DateTime.now - Constants::Shared::VALID_TOKEN_TIME) < user.created_at
+        Success(user)
+      else
+        Failure({ errors: [{ token: I18n.t('users.confirmations.token.expired') }] })
+      end
     end
 
     def update_user(user)
