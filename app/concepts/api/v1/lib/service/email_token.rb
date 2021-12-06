bf8a3976d69499fd828e05ea58bc054f301d75a1
Dandycv <dandycvc@gmail.com>
2021-12-06 17:46:57 +0200

    use HMAC_SECRET from credentials

--- app/concepts/api/v1/lib/service/email_token.rb
+++ app/concepts/api/v1/lib/service/email_token.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Api::V1::Lib::Service::EmailToken
-  HMAC_SECRET = "test123456"
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
   HMAC = 'HS256'
 
   class << self
@@ -10,7 +10,6 @@ module Api::V1::Lib::Service::EmailToken
     end
 
     def decode(email_token)
-      puts "SECRET = #{Rails.application.credentials.secret_key_base}"
       JWT.decode(email_token, HMAC_SECRET, true, { algorithm: HMAC })
     end
   end
