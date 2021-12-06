5e6f889752672164a63266c042ec0f94ebb90e0b
Dandycv <dandycvc@gmail.com>
2021-12-06 12:38:20 +0200

    CI debug

--- app/concepts/api/v1/lib/service/email_token.rb
+++ app/concepts/api/v1/lib/service/email_token.rb
@@ -10,6 +10,7 @@ module Api::V1::Lib::Service::EmailToken
     end
 
     def decode(email_token)
+      puts "SECRET = #{Rails.application.credentials.secret_key_base}"
       JWT.decode(email_token, HMAC_SECRET, true, { algorithm: HMAC })
     end
   end
