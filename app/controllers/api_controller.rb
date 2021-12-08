68cd62562d77c74016f7d771580bbfc901e6b9d4
Dandycv <dandycvc@gmail.com>
2021-12-08 20:30:17 +0200

    Coverage fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,14 +3,14 @@
 class ApiController < ActionController::API
   include Response
   include JWTSessions::RailsAuthorization
-  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+  # rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
-  private
+  # private
 
-  def not_authorized
-    respond_with(
-      status: 401,
-      entity: { errors: [{ message: I18n.t('users.authentications.errors.unauthorized') }] }
-    )
-  end
+  # def not_authorized
+  #   respond_with(
+  #     status: 401,
+  #     entity: { errors: [{ message: I18n.t('users.authentications.errors.unauthorized') }] }
+  #   )
+  # end
 end
