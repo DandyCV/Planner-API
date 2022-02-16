a9fb0676489def40d9eac96787fb3834c4a89b2a
Dandycv <dandycvc@gmail.com>
2022-02-16 12:44:19 +0200

    Add guard clause for Response module

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -4,10 +4,8 @@ module Response
   def respond_with(entity: nil, status: 200, serializer: Api::V1::Serializer::Error, options: nil)
     return head(status) unless entity
 
-    if options
-      render json: serializer.new(entity, options).to_json, status: status
-    else
-      render json: serializer.new(entity).to_json, status: status
-    end
+    return render json: serializer.new(entity).to_json, status: status unless options
+
+    render json: serializer.new(entity, options).to_json, status: status
   end
 end
