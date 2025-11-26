86f63d50ea163befd00ec363067e0c036d015b30
svelychko <dandycvc@gmail.com>
2025-11-27 00:35:15 +0200

    Add deploy to AWS workflow

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,7 +3,8 @@
 class RegistrationMailer < ApplicationMailer
   def confirmation_email(email, token, path)
     @email = email
-    @confirmation_url = URI.parse("#{path}?email_token=#{token}").to_s
+    host = Rails.application.config.action_mailer.default_url_options[:host]
+    @confirmation_url = "#{host}/#{path}?email_token=#{token}"
     mail(to: email, subject: I18n.t('user_mailer.confirmation.subject'))
   end
 end
